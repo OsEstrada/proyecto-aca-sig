5cafa82bd64ea7156a37b9862b08acf272d2873d
OsEstrada <00064318@uca.edu.sv>
2022-07-02 23:40:14 -0600

    undefined routes

--- src/services/busesRoutesUrls.js
+++ src/services/busesRoutesUrls.js
@@ -2,7 +2,6 @@ const axios = require("axios").default;
 const service = {}
 
 service.getRoutes = async (names) => {
-    console.log(names);
     try {
         const response = [];
         var route;
@@ -10,6 +9,12 @@ service.getRoutes = async (names) => {
         for (var i = 0; i < names.length; i++) {
             try {
                 let routeData = await axios.get(`https://services9.arcgis.com/4ZwMO9wShTnUDuWy/ArcGIS/rest/services/${names[i].route}/query?where=OBJECTID+>%3D+0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=+OBJECTID%2CNAME%2CTIPO_RUTA%2CRECORRIDO%2CH_INIC_LV%2CH_FIN_LV%2CH_INIC_SD%2CH_FIN_SD%2CORIGEN%2CDESTINO%2CTARIFA_AUT%2CKILOMETROS%2CCODIGO_RUTA%2CTARIFA_EXCLU%2CTIPO_UNIDAD+&returnGeometry=false&sqlFormat=none&f=pjson`);
+
+                console.log("ROUTEDATA", routeData.data);
+                if(!routeData.data.features){
+                        alert("Esta ruta no existe en la base de datos");
+                        throw 404;
+                }
 
                 route = {
                     "name": names[i].name,
